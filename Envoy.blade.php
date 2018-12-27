24dcb320010ee2828a884843fb9e65957b18a8f1
summercake@qq.com <summercake@qq.com>
2018-12-27 13:53:31 -0600

    add and config envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,8 @@
+@servers(['app-1' => ['root@68.183.164.84'], 'app-2'=>['root@68.183.164.182']])
+
+@task('deploy', ['on' => ['app-1', 'app-2'], 'parallel' => true])
+    cd /var/www/laravel-lb
+    git pull origin master
+    composer install --no-dev
+    php artisan migrate --force
+@endtask
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Load Balance Deployment
                 </div>
 
                 <div class="links">
